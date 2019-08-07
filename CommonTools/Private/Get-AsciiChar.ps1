fdb5fd29e54229ef9b7b29893b771b53b2c033f7
falkheiland <falkheiland@users.noreply.github.com>
2019-08-07 15:08:40 +0200

    Create Get-AsciiChar.ps1

---
+++ CommonTools/Private/Get-AsciiChar.ps1
@@ -0,0 +1,17 @@
+﻿function Get-AsciiChar
+<#
+  .Example
+  Get-AsciiChar -Ascii (48..57)
+#>
+{
+  param
+  (
+    [Parameter(Mandatory)]$Ascii
+  )
+
+  $Ascii |
+    Get-Random |
+    ForEach-Object -Process {
+      [char]$_
+    }
+}
